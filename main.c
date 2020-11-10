6841bca561bfd6b60552f983bc975faed0465d13
koadegno <vinove9@hotmail.fr>
2020-11-10 21:26:45 +0100

    [CODE] test

--- main.c
+++ main.c
@@ -4,6 +4,20 @@
 #include <stdio.h>
 
 int main(int argc, char const *argv[]) {
-    printf("Good luck in this projet!\n");
+
+    char* student_file;
+
+    if(argc > 3) return 1;
+
+    if(argc>2) student_file = agrv[2];
+
+
+    printf("file %s : ",student_file)
+
+
+
+
+
+
     return 0;
 }

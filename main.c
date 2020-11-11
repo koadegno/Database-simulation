079b6dcff59ef8ba6891c83d14984a471355b99c
koadegno <vinove9@hotmail.fr>
2020-11-11 20:20:53 +0100

    [CODE] open file correctly

--- main.c
+++ main.c
@@ -11,22 +11,29 @@ int open_file(const char* file_name,student_t* obj_student){
     FILE* file_data;
     file_data = fopen(file_name,"rb");
     
-    printf("tailel de unsigned : %u",( unsigned int )UINT_MAX);
+    
+  
+    // calculating the size of the file 
+     
+    
 
     if(!file_data) {fclose(file_data); return 0;} //false
 
-    while(file_data){
-        //fseek(file_data, -sizeof(student_t)*3, SEEK_END);
+    //long int a= fseek(file_data, -sizeof(student_t)*10, SEEK_END);
+    fseek(file_data, 0L, SEEK_END); 
+    long int size = ftell(file_data)/sizeof(student_t);
+    fseek(file_data, sizeof(student_t), SEEK_SET);
+
+    for(int i=0;i <size; i++)
+    {
+        //
         printf("\n");
         fread(obj_student,sizeof(*obj_student),1,file_data);
         printf("id : %u\nFirst name : %s\nLast name : %s\nSection : %s\nAnnif : %d/%d/%d\n",obj_student->id,obj_student->fname,obj_student->lname,obj_student->section,obj_student->birthdate.tm_mday,obj_student->birthdate.tm_mon,obj_student->birthdate.tm_year);
+        
     };
 
-    fseek(file_data, 0L, SEEK_END); 
-  
-    // calculating the size of the file 
-    long int res = ftell(file_data); 
-    printf("Taille : %ld\n",res);
+    
 
     fclose(file_data);
     return 1; //true
